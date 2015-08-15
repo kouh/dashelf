47118a99fa4ac7121c7fe52c80e61458d4ca59d3
kouh <kouh28@gmail.com>
2015-08-16 00:11:50 +0900

    リンク登録機能追加

--- dashelf.js
+++ dashelf.js
@@ -1,7 +1,24 @@
 'use strict'
 
+const shell = require('shell');
 let dashboard = document.getElementById('dash-board');
-let addBtn = document.getElementById(
+let addBtn = document.getElementById('add-btn');
+
+
+dashboard.addEventListener('click', function(e){
+  if(e.target.href.match(/^http:\/\//)) {
+    shell.openExternal(e.target.getAttribute('href'));
+    e.preventDefault();
+    return false;
+  }
+});
+
+addBtn.addEventListener('click', function(){
+  let text = document.getElementById('text-input').value;
+  dashboard.innerHTML += `
+    <a href="${text}" class="item">${text}</a>
+  `;
+});
 
 document.ondragover = function () {
   return false;
@@ -15,8 +32,8 @@ document.ondrop = function (e) {
   e.preventDefault();
 
   let file = e.dataTransfer.files[0];
-  dashboard.innerHTML = `
-    <a href="${file.path}">${file.name}</a>
+  dashboard.innerHTML += `
+    <a href="${file.path}" class="item">${file.name}</a>
   `;
 
   return false;
