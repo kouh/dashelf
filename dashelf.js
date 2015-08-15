a59b6de6b7ce12279a085384f1810a9d4b5cc4e0
kouh <kouh28@gmail.com>
2015-08-15 23:28:05 +0900

    js修正

--- dashelf.js
+++ dashelf.js
@@ -1,1 +1,24 @@
+'use strict'
+
 console.log('hello world');
+let dashboard = document.getElementById('dash-board');
+
+document.ondragover = function () {
+  return false;
+};
+
+document.ondragleave = document.ondragend = function () {
+  return false;
+};
+
+document.ondrop = function (e) {
+  e.preventDefault();
+
+  var file = e.dataTransfer.files[0];
+  dashboard.innerHTML = `
+    <a href="${file.path}">${file.name}</a>
+  `;
+  // console.log(file.path);
+
+  return false;
+};
